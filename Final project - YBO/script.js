c74f774c8b67dcbc3dc0f4913c0631daad1229ca
YBOט <yitzhak851@gmail.com>
2022-04-30 15:54:10 +0300

    Create a folder and files for the concluding project

---
+++ Final project - YBO/script.js
@@ -0,0 +1,39 @@
+const MAX_VALUE = 'max'
+
+//variables types :
+// - object
+// - array
+// - boolean
+// - integer
+// - string
+
+
+/*
+
+loop - while
+
+
+if (condition) {
+
+}
+else {
+
+}
+return;
+
+
+loop - for
+
+
+anonymus function
+
+Events
+
+
+
+DOM
+
+
+
+
+*/
