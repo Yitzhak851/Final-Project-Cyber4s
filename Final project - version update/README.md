0f8ca59c1269cbf68c1a39e5b1efe8090737f906
YBOט <yitzhak851@gmail.com>
2022-05-03 02:12:23 +0300

    Correction of the link to the project - in the README file and adding constants and variables in the JS file

--- Final project - version update/README.md
+++ Final project - version update/README.md
@@ -2,4 +2,4 @@
 
 This 'repo' represents my final project on the 'pre-course' of 'Cyber 4S' :)
 
-[Link](https://htmlpreview.github.io/?http://127.0.0.1:5500/index.html) for quick viewing of the final product:
+[Link](https://htmlpreview.github.io/?http://127.0.0.1:5500/) for quick viewing of the final product:
---
+++ Final project - version update/script.js
@@ -0,0 +1,11 @@
+//const area
+
+const cells = document.querySelectorAll("td");
+let whitePieces = document.querySelectorAll("p");
+let blacksPieces = document.querySelectorAll("span")
+const whiteTurnText = document.querySelectorAll(".white-turn-text");
+const blackTurntext = document.querySelectorAll(".black-turn-text");
+const divider = document.querySelector("#divider");
+
+
+//TODO - Add functions to create an interactive checkers game
