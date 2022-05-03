0e099cdd8a2de70a5b3af257153259e74511f6c3
YBOט <yitzhak851@gmail.com>
2022-05-03 11:53:30 +0300

    Adding even listener & selected piece properties & option to find the piece location

--- Final project - version update/script.js
+++ Final project - version update/script.js
@@ -1,4 +1,5 @@
-//const area
+/*----------const area---------- */
+//Create game status data in the form of a list
 const board = [
     null, 0, null, 1, null, 2, null, 3,
     4, null, 5, null, 6, null, 7, null,
@@ -10,6 +11,7 @@ const board = [
     20, null, 21, null, 22, null, 23, null
 ]
 
+/*----------Storage of variables - DOM---------- */
 const cells = document.querySelectorAll("td");
 let whitePieces = document.querySelectorAll("p");
 let blacksPieces = document.querySelectorAll("span")
@@ -17,8 +19,47 @@ const whiteTurnText = document.querySelectorAll(".white-turn-text");
 const blackTurntext = document.querySelectorAll(".black-turn-text");
 const divider = document.querySelector("#divider");
 
-// General characteristics for players - propertiesץ
+// General characteristics for players - assets
 let turn = true;
 let whiteScore = 12;
 let blackScore = 12;
 let playerPieces;
+
+// Analyze piece location (Id) and returns the index of that piece place on the checkers- board
+let findPiece;
+findPiece = function (pieceId) {
+    let parsed = parseInt(pieceId);
+    return board.indexOf(parsed);
+};
+
+// selected piece properties
+let selectedPiece = {
+    pieceId: -1,
+    indexOfBoardPiece: -1,
+    isKing: false,
+    seventhSpace: false,
+    ninthSpace: false,
+    fourteenthSpace: false,
+    eighteenthSpace: false,
+    minusSeventhSpace: false,
+    minusNinthSpace: false,
+    minusFourteenthSpace: false,
+    minusEighteenthSpace: false
+}
+
+/*---------- Adding - event listeners ----------*/
+
+function givePiecesEventListeners() {
+    if (turn) {
+        for (let i = 0; i < whitePieces.length; i++) {
+            whitePieces[i].addEventListener("click", getPlayerPieces);
+        }
+    } else {
+        for (let i = 0; i < blacksPieces.length; i++) {
+            blacksPieces[i].addEventListener("click", getPlayerPieces);
+        }
+        console.log('hi man, its work !')
+    }
+}
+
+givePiecesEventListeners();
